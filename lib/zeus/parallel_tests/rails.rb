1cd2425eebb004161d049a867c2e6c64bc58f58c
Artur Roszczyk <artur.roszczyk@gmail.com>
2013-03-26 17:52:42 +0100

    Add cucumber support. Fixes #5

--- lib/zeus/parallel_tests/rails.rb
+++ lib/zeus/parallel_tests/rails.rb
@@ -3,6 +3,40 @@ require_relative '../parallel_tests'
 module Zeus
   module ParallelTests
     class Rails < ::Zeus::Rails
+      def parallel_cucumber
+        executable = %[PARALLEL_TESTS_EXECUTABLE='ruby "#{File.expand_path("../worker/cucumber.rb", __FILE__)}"']
+        exec "#{executable} parallel_cucumber #{ARGV.join(' ')}"
+      end
+
+      def parallel_cucumber_worker
+        argv = ARGV.dup
+        test_env_number = argv.shift
+        # Parallels spec reuse main test db instead of db with "1" appended
+        test_env_number = nil if test_env_number == "1"
+
+        return unless defined?(ActiveRecord::Base)
+        begin
+          ActiveRecord::Base.clear_all_connections!
+
+          config = ActiveRecord::Base.connection_config
+          config[:database] = "#{config[:database]}#{test_env_number}"
+
+          ActiveRecord::Base.establish_connection(config)
+          if ActiveRecord::Base.respond_to?(:shared_connection)
+            ActiveRecord::Base.shared_connection = ActiveRecord::Base.retrieve_connection
+          end
+        rescue ActiveRecord::AdapterNotSpecified
+        end
+
+        ENV['TEST_ENV_NUMBER'] = test_env_number
+        ENV['PARALLEL_TEST_GROUPS'] = argv.shift
+
+        rspec_args_file = argv.shift
+        test_files = File.readlines(rspec_args_file).map(&:chomp)
+
+        cucumber(test_files)
+      end
+
       def parallel_rspec
         executable = %[PARALLEL_TESTS_EXECUTABLE='ruby "#{File.expand_path("../worker/rspec.rb", __FILE__)}"']
         exec "#{executable} parallel_rspec #{ARGV.join(' ')}"
@@ -45,6 +79,13 @@ module Zeus
         else
           Zeus::M.run(argv)
         end
+      end
+
+      def cucumber(argv = ARGV)
+        cucumber_main = Cucumber::Cli::Main.new(argv.dup)
+        had_failures = cucumber_main.execute!(@cucumber_runtime)
+        exit_code = had_failures ? 1 : 0
+        exit exit_code
       end
 
     end
